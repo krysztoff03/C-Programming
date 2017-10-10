3c745c1d932fb2bc163fbb1f87820f444cf9dcb1
krysztoff03 <cristi.iosif03@gmail.com>
2017-10-10 20:01:22 +0300

    Add files via upload

---
+++ Introduction/Multiple.c
@@ -0,0 +1,20 @@
+
+/*
+* Program that reads in two integers and determines and prints
+* whether the first is a multiple of the second.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    printf("enter two integers: ");
+    scanf("%d%d", &a, &b);
+    if(a % b == 0)
+        printf("%d is a multiple of %d\n", a, b);
+    if(a % b != 0)
+        printf("%d is not a multiple of %d\n", a, b);
+    return 0;
+}
