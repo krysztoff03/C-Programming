1b4c78bdf3683887fb50bee5be5664c9ada6dd82
krysztoff03 <cristi.iosif03@gmail.com>
2017-10-10 19:55:44 +0300

    Add files via upload

---
+++ Introduction/OddOrEven.c
@@ -0,0 +1,20 @@
+
+/*
+* Program that reads an integer and determines and prints whether
+* it’s odd or even.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("enter an integer: ");
+    scanf("%d", &a);
+    if(a % 2 == 0)
+        printf("the number is even.\n");
+    else
+        printf("the number is odd.\n");
+    return 0;
+}
