6cebe7a5b370d5202e6d073e6c8840c18d13035d
krysztoff03 <cristi.iosif03@gmail.com>
2017-10-11 18:56:57 +0300

    Add files via upload

--- Structured Program Development/SalaryCalculator.c
+++ Structured Program Development/SalaryCalculator.c
@@ -1,12 +1,13 @@
 
 /*
-* One large chemical company pays its salespeople on a commission
-* basis. The salespeople receive $200 per week plus 9% of their gross
-* sales for that week. For example, a salesperson who sells $5000 worth
-* of chemicals in a week receives $200 plus 9% of $5000, or a total of
-* $650. Develop a program that will input each salesperson’s gross sales
-* for last week and will calculate and display that salesperson’s earnings.
-* Process one salesperson's figures at a time.
+* Program that will determine the gross pay for each of several
+* employees. The company pays “straight time” for the first 40 hours
+* worked by each employee and pays “time-and-a-half” for all hours
+* worked in excess of 40 hours. You’re given a list of the employees
+* of the company, the number of hours each employee worked last week
+* and the hourly rate of each employee. Your program should input this
+* information for each employee and should determine and display the
+* employee's gross pay.
 */
 
 #include <stdio.h>
@@ -14,15 +15,24 @@
 
 int main()
 {
-    double sales, totalSalary;
-    printf("enter sales in dollars (-1 to end): $");
-    scanf("%lf", &sales);
-    while(sales != -1)
+    int hours;
+    double hourlyRate;
+    printf("enter # of hours worked (-1 to end): ");
+    scanf("%d", &hours);
+    while(hours != -1)
     {
-        totalSalary = 200 + 0.09 * sales;
-        printf("salary is: $%.2f\n", totalSalary);
-        printf("\nenter sales in dollars (-1 to end): $");
-        scanf("%lf", &sales);
+        printf("enter hourly rate of the worker ($00.00): ");
+        scanf("%lf", &hourlyRate);
+        if(hours <= 40)
+        {
+            printf("salary is $%.2f\n", hours * hourlyRate);
+        }
+        else
+        {
+            printf("salary is $%.2f\n", (40 * hourlyRate) + (hours - 40) * 15);
+        }
+        printf("\nenter # of hours worked (-1 to end): ");
+        scanf("%d", &hours);
     }
     return 0;
 }
