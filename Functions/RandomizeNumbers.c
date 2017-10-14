a39fa534917cb4711519a98e3c744a5c8f58297e
krysztoff03 <cristi.iosif03@gmail.com>
2017-10-14 15:13:49 +0300

    Add files via upload

--- Functions/RandomizeNumbers.c
+++ Functions/RandomizeNumbers.c
@@ -10,19 +10,34 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+int randSet1();
+int randSet2();
+int randSet3();
 
 int main()
 {
-    int a, b, c;
     srand(time(NULL));
-    a = (1 + rand() % 5) * 2;
-    b = ((1 + rand() % 5) * 2) + 1;
-    c = ((1 + rand() % 5) + 1) * 2;
-    printf("%d\n", a);
-    printf("%d\n", b);
-    printf("%d\n", c);
+    printf("Set 1: %d\n", randSet1());
+    printf("Set 2: %d\n", randSet2());
+    printf("Set 3: %d\n", randSet3());
     return 0;
 }
-
+int randSet1()
+{
+    int x;
+    x = (1 + rand() % 5) * 2;
+    return x;
+}
+int randSet2()
+{
+    int x;
+    x = ((1 + rand() % 5) * 2) + 1;
+    return x;
+}
+int randSet3()
+{
+    int x;
+    x = (((1 + rand() % 5) * 2) + 1) * 2;
+    return x;
+}
 
